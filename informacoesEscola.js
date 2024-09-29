a919e57de829b8c6a14308a97ae816bf0f19c742
DayaneOie <165847194+DayaneOie@users.noreply.github.com>
2024-09-29 17:20:43 -0300

    Update informacoesEscola.js

--- informacoesEscola.js
+++ informacoesEscola.js
@@ -13,9 +13,9 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `
-    Você sabia que a escola possui aproximadamente <span style="color: ${corAleatoria};">2.000 alunos</span>? Dentre esses, cerca de <span style="color: ${corAleatoria};">${alunosConectados} alunos</span> estão ativos em redes sociais. <br>
-    Em média, cada aluno dedica cerca de <span style="color: ${corAleatoria};">${horas} horas</span> e <span style="color: ${corAleatoria};">${minutos} minutos</span> por dia navegando nessas plataformas. <br>
-    Isso representa aproximadamente <span style="color: ${corAleatoria};">${porcentagemConectada}%</span> dos alunos da escola conectados às redes sociais.
+    Você sabia que a escola possui aproximadamente <span style="color: ${corAleatoria};">2.000 alunos</span>? Dentre esses, cerca de <span style="color: ${corAleatoria};">${alunosConectados} alunos</span> estão ativos em redes sociais por conta da maquiagem. <br>
+    Em média, cada aluno dedica cerca de <span style="color: ${corAleatoria};">${horas} horas</span> e <span style="color: ${corAleatoria};">${minutos} minutos</span> por dia navegando nessas plataformas atrás de novas dicas e inspirações. <br>
+    Isso representa aproximadamente <span style="color: ${corAleatoria};">${porcentagemConectada}%</span> dos alunos da escola conectados à essas redes sociais por esse motivo.
 `;
 
 
